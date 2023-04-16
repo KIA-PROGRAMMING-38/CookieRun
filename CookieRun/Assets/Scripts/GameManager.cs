417488e29c4fdb9687c6f0a3b6d55641e8518e15
Huiji_MacMini <gmlwl0408@gmail.com>
2023-04-17 03:11:31 +0900

    [Refac] GameManager 리팩토링

--- CookieRun/Assets/Scripts/GameManager.cs
+++ CookieRun/Assets/Scripts/GameManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static bool gameOver;
-    public static float score;
+    private static float score;
     
     private void Awake()
     {
         gameOver = false;
     }
 
+    // 점수를 추가하는 메소드
     public static void UpdateScore(float scoreToAdd)
     {
         score += scoreToAdd;
