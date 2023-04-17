da11e7c0a17a76f74ab3a6d2c646ef157465783c
Huiji_MacMini <gmlwl0408@gmail.com>
2023-04-17 16:59:02 +0900

    [Feat] EnergyPotion 구현

---
+++ CookieRun/Assets/Scripts/EnergyPotion.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyPotion : MonoBehaviour
+{
+    private PlayerController _playerController;
+    public float hpRecoveryValue = 10f;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            // 플레이어의 컴포넌트를 가져온다.
+            _playerController = col.GetComponent<PlayerController>();
+            
+            // hp 회복
+            _playerController.ChangeHp(hpRecoveryValue);
+            Debug.Log(PlayerData.HP);
+            
+            gameObject.SetActive(false);
+        }
+    }
+}
--- CookieRun/Assets/Scripts/PlayerData.cs
+++ CookieRun/Assets/Scripts/PlayerData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,4 +15,9 @@ public class PlayerData : MonoBehaviour
     public static float HP = 50f;
 
     public bool jumping = false;
+
+    private void Start()
+    {
+        Debug.Log(HP);
+    }
 }
