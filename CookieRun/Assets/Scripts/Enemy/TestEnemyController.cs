f135d8b544b2b59446a8f55b9cadc7ab26807ef1
Huiji_MacMini <gmlwl0408@gmail.com>
2023-04-26 20:08:52 +0900

    [Feat] Enemy Sprite Resources 불러오기

--- CookieRun/Assets/Scripts/Enemy/TestEnemyController.cs
+++ CookieRun/Assets/Scripts/Enemy/TestEnemyController.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EnemyAnimationID;
 
 public class TestEnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Enemy _enemyData;
+    
+    private PlayerData _playerData;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+
+    private void Awake()
     {
-        
+        _animator = GetComponent<Animator>();
+        Activate(EnemyKind.LarvaSmall);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Activate(EnemyKind enemyKind)
     {
-        
+        _enemyData = DataManager.Enemies[(int)enemyKind];
+        // BindData
+    }
+
+    private void BindData()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+
+        string spriteName = $"enemy_{_enemyData.SpriteName}";
+
+        _spriteRenderer.sprite = DataManager.LoadEnemySprite(spriteName);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        _playerData = col.GetComponent<PlayerData>();
+
+        if (_playerData != null && _playerData.isLightSpeed)
+        {
+            _animator.SetTrigger(EnemyAnimID.IS_EXPLOSION);
+        }
+    }
+
+    public void SetIdleAnimation()
+    {
+        _animator.Play("Idle");
     }
 }
