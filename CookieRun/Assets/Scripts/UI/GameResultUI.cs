d847bb43853bf64b9265944397d4bfca02371dec
Huiji_MacMini <gmlwl0408@gmail.com>
2023-04-28 20:39:57 +0900

    [Feat] GameOver -> 2초뒤 GameResultUI

--- CookieRun/Assets/Scripts/UI/GameResultUI.cs
+++ CookieRun/Assets/Scripts/UI/GameResultUI.cs
@@ -2,30 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameResultUI : MonoBehaviour
 {
     [SerializeField] private float _waitSeconds = 3f;
-
-    private float _startTime;
+    private Image _image;
+    private IEnumerator _showUICoroutine;
+    private WaitForSeconds _waitForSeconds;
+    
     private void Awake()
     {
         GameManager.OnGameEnd -= Ativate;
         GameManager.OnGameEnd += Ativate;
-        
-        _startTime = Time.time;
+        _image = GetComponentInChildren<Image>();
+        _waitForSeconds = new WaitForSeconds(_waitSeconds);
     }
 
     private void Start()
     {
+        _image.enabled = false;
         gameObject.SetActive(false);
     }
 
+    private void OnEnable()
+    {
+        if (_showUICoroutine != null)
+        {
+            StopCoroutine(_showUICoroutine);
+        }
+
+        _showUICoroutine = ShowUI();
+        StartCoroutine(_showUICoroutine);
+    }
+
+    IEnumerator ShowUI()
+    {
+        yield return _waitForSeconds;
+        _image.enabled = true;
+    }
     private void Ativate()
     {
-        if (Time.fixedTime >= _startTime + _waitSeconds)
-        {
-            gameObject.SetActive(true);
-        }
+        gameObject.SetActive(true);
     }
 }
